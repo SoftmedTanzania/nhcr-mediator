aadf95aa4e75cb2f248129b043cf16ee29eb6809
Nityan Khanna <nityan.khanna@gmail.com>
2021-02-12 15:09:43 -0500

    added an HL7v2 message builder class

---
+++ src/main/java/tz/go/moh/him/nhcr/mediator/utils/HL7v2MessageBuilderUtils.java
@@ -0,0 +1,17 @@
+package tz.go.moh.him.nhcr.mediator.utils;
+
+import ca.uhn.hl7v2.model.v231.message.ADT_A04;
+
+/**
+ * Represents an HL7v2 message builder utility.
+ */
+public class HL7v2MessageBuilderUtils {
+
+    /**
+     * Creates an ADT A04 message.
+     * @return Returns the created ADT A04 message.
+     */
+    public static ADT_A04 createAdtA04() {
+        return new ADT_A04();
+    }
+}
